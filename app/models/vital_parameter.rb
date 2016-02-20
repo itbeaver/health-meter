b41758648aa3c8894cc383bc97a668f168dd79ed
Denis Erofeev <edenisn@gmail.com>
2016-02-20 18:51:52 +0300

    Remove self attr from VitalParameter model

--- app/models/vital_parameter.rb
+++ app/models/vital_parameter.rb
@@ -1,24 +1,24 @@
 class VitalParameter < ActiveRecord::Base
 
   def calculate_vegetative_index
-    (1 - self.diastolic_pressure / self.heart_rate) * 100
+    (1 - diastolic_pressure / heart_rate) * 100
   end
 
   def calculate_endurance_factor
-    (self.heart_rate * self.systolic_blood_pressure * 10) / self.diastolic_pressure
+    (heart_rate * systolic_blood_pressure * 10) / diastolic_pressure
   end
 
   def calculate_blood_circulation
-    (self.systolic_blood_pressure - self.diastolic_pressure) * self.heart_rate
+    (systolic_blood_pressure - diastolic_pressure) * heart_rate
   end
 
   def calculate_robinson_index
-    self.heart_rate * self.systolic_blood_pressure / 100
+    heart_rate * systolic_blood_pressure / 100
   end
 
   def calculate_physical_condition
-    middle_pressure = self.diastolic_pressure + (self.systolic_blood_pressure - self.diastolic_pressure) / 3
-    physical_condition = (700 - 3 * self.heart_rate - 2.5 * middle_pressure - 2.7 * self.age + 0.28 * self.weight) /
-        (350 - 2.6 * self.age + 0.21 * self.height)
+    middle_pressure = diastolic_pressure + (systolic_blood_pressure - diastolic_pressure) / 3
+    physical_condition = (700 - 3 * heart_rate - 2.5 * middle_pressure - 2.7 * age + 0.28 * weight) /
+        (350 - 2.6 * age + 0.21 * height)
   end
 end
