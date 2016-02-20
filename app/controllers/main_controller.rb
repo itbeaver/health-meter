11fcfcbc339e36f39d352a62b0340c959253d026
Denis Erofeev <edenisn@gmail.com>
2016-02-20 12:24:11 +0300

    Add HealthController with views to repo

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,5 @@
 class MainController < ApplicationController
   def index
+    redirect_to controller: 'health', action: 'index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'main#index'
 
+  resources :health
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
